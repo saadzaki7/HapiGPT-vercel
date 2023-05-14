77041a0cdab391a3c74ded67a9744cc99168ae25
Saad Zaki <65866779+saadzaki7@users.noreply.github.com>
2023-05-13 21:27:39 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       <div className="gradient-02 z-0 absolute"></div>
 
       {/* header */}
-      <div className="uppercase font-bold  text-2xl text-center mb-3">
+      <div className="font-bold  text-5xl text-center mb-3">
         HapiGPT
       </div>
 
